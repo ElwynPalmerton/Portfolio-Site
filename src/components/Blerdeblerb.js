c0b4be262e92af3a1d206b33a47437d2177ef770
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2023-01-05 01:35:10 -0800

    Change Blerdeblerb to Blerdeblurb

--- src/components/Blerdeblerb.js
+++ src/components/Blerdeblerb.js
@@ -1,46 +1,33 @@
-import React from 'react';
-import Description from './Description';
-import PageWrapper from './PageWrapper';
+import React from "react";
+import Description from "./Description";
+import PageWrapper from "./PageWrapper";
 
 const text = {
-  name: "BLERDEBLERB🗯",
+  name: "BLERDEBLURB🗯",
   font: "blerbFont",
-  url: "https://www.blerdeblerb.com/",
+  url: "https://www.blerdeblurb.com/",
   imgUrl: "https://i.ibb.co/LRcL8Fn/Blerdeblerb-screenshot.png",
   description: " Blerdeblerb is a Twitter-clone.",
   tech: [
-    "React with Redux and React Router"
-    , "Node.js and Express"
-    , "MongoDB and Mongoose"
-    , "Axios for data fetching"
-    , "Material-UI"
-    , "JSON web tokens"
-    , "Multer and Sharp for image uploading."
+    "React with Redux and React Router",
+    "Node.js and Express",
+    "MongoDB and Mongoose",
+    "Axios for data fetching",
+    "Material-UI",
+    "JSON web tokens",
+    "Multer and Sharp for image uploading.",
   ],
   features: [
     "Twitter-like functionality",
     "Post blerbs to your feed.",
     "Follow your friends' blerbs",
     "LIKE and REBLERB your friends' blerbs",
-    "Customize your profile with a user image and bio"
-  ]
-}
-
+    "Customize your profile with a user image and bio",
+  ],
+};
 
 function Blerdeblerb() {
-
-  return (
-
-    <PageWrapper
-      center={
-        <Description
-          text={text}
-
-        />
-      } />
-  )
+  return <PageWrapper center={<Description text={text} />} />;
 }
 
 export default Blerdeblerb;
-
-
--- src/constants.js
+++ src/constants.js
@@ -7,17 +7,16 @@ const constants = [
       "Zybriqs is an interactive ball and block animation. Zybriqs was made using P5, Javascript, Express, Express-sessions, and MongoDB. The animation is created in p5.js. It models realistic movement using the physics equations for gravity, wind, and acceleration to model the movement of a ball.",
     link: "http://www.zybriqs.com/game",
     imageUrl:
-      "https://i.ibb.co/ZYDyNnC/Screen-Shot-2020-12-04-at-3-26-08-PM.png"
+      "https://i.ibb.co/ZYDyNnC/Screen-Shot-2020-12-04-at-3-26-08-PM.png",
   },
   {
-    title: "blerdeblerb",
-    route: "blerdeblerb",
+    title: "blerdeblurb",
+    route: "blerdeblurb",
     fontType: "blerbFont",
     description:
       "blerdeblerb is a Twitter clone made using React, Express, Node, and MongoDB. blerdeblerb allows users to post blerbs and reblerb or like other users blerbs. blerdeberb also uses router-dom, react redux, axios, and react-image.",
-    link: "https://www.blerdeblerb.com/",
-    imageUrl:
-      "https://i.imgur.com/D6pHmGp.jpg"
+    link: "https://www.blerdeblurb.com/",
+    imageUrl: "https://i.imgur.com/D6pHmGp.jpg",
   },
 ];
 
@@ -30,7 +29,7 @@ const constants2 = [
       "zOrbitals is an animation made with p5.js. It models gravitational force for the zOrbitals. zOrbitals also has a built-in sequencer for timed events",
     link: "https://elwynpalmerton.github.io/zOrbitals/",
     imageUrl:
-      "https://i.ibb.co/YTbzxf8/Screen-Shot-2020-12-10-at-2-02-42-AM.png"
+      "https://i.ibb.co/YTbzxf8/Screen-Shot-2020-12-10-at-2-02-42-AM.png",
   },
   {
     title: "Trampoline",
@@ -40,19 +39,18 @@ const constants2 = [
       "An animation using p5.js and the toxiclibs - a physics engine for modeling spring and attraction forces.",
     link: "https://elwynpalmerton.github.io/toxiclibs/",
     imageUrl:
-      "https://i.ibb.co/mJDYfjh/Screen-Shot-2021-02-15-at-3-05-26-PM.png"
+      "https://i.ibb.co/mJDYfjh/Screen-Shot-2021-02-15-at-3-05-26-PM.png",
   },
   {
     title: "Walker",
     route: "walker",
     fontType: "walker",
-    description:
-      "A walker animation using p5.js",
+    description: "A walker animation using p5.js",
     link: "https://elwynpalmerton.github.io/walker/",
     imageUrl:
-      "https://i.ibb.co/QKbPhdr/Screen-Shot-2021-01-04-at-5-47-47-PM.png"
+      "https://i.ibb.co/QKbPhdr/Screen-Shot-2021-01-04-at-5-47-47-PM.png",
   },
-]
+];
 
 const constants3 = [
   {
@@ -63,8 +61,8 @@ const constants3 = [
       "Automatically generates an AVL tree with a random number of nodes with random values.",
     link: "https://elwynpalmerton.github.io/AVL-Tree/",
     imageUrl:
-      "https://i.ibb.co/nb8Rvhk/Screen-Shot-2021-01-04-at-6-17-11-PM.png"
+      "https://i.ibb.co/nb8Rvhk/Screen-Shot-2021-01-04-at-6-17-11-PM.png",
   },
-]
+];
 
-export { constants as default, constants2, constants3 }
+export { constants as default, constants2, constants3 };
