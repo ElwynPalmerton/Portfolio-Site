7b6edcd5f6fa19b2a4c766f6778d81f1ea9b4710
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2023-01-10 18:17:26 -0800

    Fix links

--- src/components/Zybriqs.js
+++ src/components/Zybriqs.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import Description from './Description';
-import PageWrapper from './PageWrapper';
-
+import React from "react";
+import Description from "./Description";
+import PageWrapper from "./PageWrapper";
 
 const text = {
   name: "Zybriqs",
   font: "zybriqsTitle",
-  url: "http://www.zybriqs.com/game",
+  url: "http://www.zybriqs.com",
   imgUrl: "https://i.ibb.co/ZYDyNnC/Screen-Shot-2020-12-04-at-3-26-08-PM.png",
   description: " Zybriqs is an interactive ball and block animation.",
   tech: [
-    "p5.js"
-    , "Javascript"
-    , "Node.js and Express"
-    , "Express Sessions"
-    , "Passport.js"
-    , "MongoDB and Mongoose"
-    , "EJS",
-    , "BootStrap"
+    "p5.js",
+    "Javascript",
+    "Node.js and Express",
+    "Express Sessions",
+    "Passport.js",
+    "MongoDB and Mongoose",
+    "EJS",
+    ,
+    "BootStrap",
   ],
   features: [
     "A unique interactive animation which models gravity, wind, acceleration, and drag.",
@@ -26,22 +26,11 @@ const text = {
     "Draw blocks, accelerators, and drag areas on the Zybriqs canvas.",
     "Activate several preset arrangements",
     "Play God by changing the strength and direction of gravitational force.",
-  ]
-}
+  ],
+};
 
 function Zybriqs() {
-
-  return (
-    <PageWrapper
-      center={
-        <Description
-          text={text}
-
-        />
-      } />
-  )
+  return <PageWrapper center={<Description text={text} />} />;
 }
 
 export default Zybriqs;
-
-
--- src/constants.js
+++ src/constants.js
@@ -5,13 +5,13 @@ const constants = [
     fontType: "zybriqsFont",
     description:
       "Zybriqs is an interactive ball and block animation. Zybriqs was made using P5, Javascript, Express, Express-sessions, and MongoDB. The animation is created in p5.js. It models realistic movement using the physics equations for gravity, wind, and acceleration to model the movement of a ball.",
-    link: "http://www.zybriqs.com/game",
+    link: "http://www.zybriqs.com",
     imageUrl:
       "https://i.ibb.co/ZYDyNnC/Screen-Shot-2020-12-04-at-3-26-08-PM.png",
   },
   {
     title: "blerdeblurb",
-    route: "blerdeblurb",
+    route: "blerdeblerb",
     fontType: "blerbFont",
     description:
       "blerdeblerb is a Twitter clone made using React, Express, Node, and MongoDB. blerdeblerb allows users to post blerbs and reblerb or like other users blerbs. blerdeberb also uses router-dom, react redux, axios, and react-image.",
